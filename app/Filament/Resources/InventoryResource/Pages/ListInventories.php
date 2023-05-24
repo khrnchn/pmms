12f2ea0b54bac1ea3a1a465e6e9bd041d57ccbe8
Khairin Chan <khairin13chan@gmail.com>
2023-05-24 16:20:57 +0800

    populate yesterday's stock data into today's closing form.

--- app/Filament/Resources/InventoryResource/Pages/ListInventories.php
+++ app/Filament/Resources/InventoryResource/Pages/ListInventories.php
@@ -6,7 +6,10 @@ use AlperenErsoy\FilamentExport\Actions\FilamentExportBulkAction;
 use AlperenErsoy\FilamentExport\Actions\FilamentExportHeaderAction;
 use App\Filament\Resources\InventoryResource;
 use App\Filament\Resources\InventoryResource\Widgets\InventoryOverview;
+use App\Models\DailyStock;
 use App\Models\Inventory;
+use App\Models\Sales;
+use App\Models\SalesItem;
 use App\Models\User;
 use Filament\Forms\Components\Grid;
 use Filament\Forms\Components\Select;
@@ -15,11 +18,15 @@ use Filament\Notifications\Notification;
 use Filament\Pages\Actions;
 use Filament\Resources\Pages\ListRecords;
 use Awcodes\FilamentTableRepeater\Components\TableRepeater;
+use Carbon\Carbon;
+use Closure;
 use Filament\Forms\Components\Card;
 use Filament\Forms\Components\Fieldset;
 use Filament\Forms\Components\Group;
+use Filament\Forms\Components\Hidden;
 use Filament\Forms\Components\Placeholder;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Str;
 
 class ListInventories extends ListRecords
 {
@@ -41,37 +48,89 @@ class ListInventories extends ListRecords
                     $livewire->redirect(InventoryResource::getURL('index'));
                 })
                 ->form([
-                    TableRepeater::make('dailystocks')
-                        ->defaultItems(Inventory::count())
+                    TextInput::make('date')
+                        ->hidden()
+                        ->default(today())
+                        ->afterStateHydrated(function ($set) {
+                            $inventories = Inventory::all()->pluck('name', 'id');
+                            $data = [];
+                            foreach ($inventories as $key => $val) {
+                                $item = [];
+
+                                $item['inventory_id'] = $key;
+
+                                $item['name'] = $val;
+
+                                $start = Carbon::yesterday()->startOfDay();
+                                $end = Carbon::yesterday()->endOfDay();
+                                $item['opening'] = DailyStock::where([
+                                    'inventory_id' => $key,
+                                ])->whereBetween('created_at', [$start, $end])
+                                    ->value('after');
+
+                                $item['sold'] = SalesItem::where([
+                                    'inventory_id' => $key,
+                                ])->whereBetween('created_at', [$start, $end])
+                                    ->value('qty');
+
+                                array_push($data, $item);
+                            }
+
+                            // set the table repeater with the data
+                            $set('stockArray', $data);
+                        }),
+
+                    TableRepeater::make('stockArray')
+                        ->default(function ($get) {
+                            $stockArray = $get('stockArray');
+                            $defaultItems = [];
+
+                            if (is_array($stockArray)) {
+                                foreach ($stockArray as $item) {
+                                    $defaultItems[] = [
+                                        'inventory_id' => $item['inventory_id'],
+                                        'name' => $item['name'],
+                                        'before' => $item['opening'],
+                                        'sold' => $item['sold'],
+                                    ];
+                                }
+                            }
+
+                            return $defaultItems;
+                        })
+                        ->disableItemCreation()
                         ->disableItemDeletion()
+                        ->disableItemMovement()
                         ->label(__('Daily stock record'))
                         ->relationship('dailyStocks')
                         ->hideLabels()
                         ->schema([
-                            Select::make('inventory_id')
+                            Hidden::make('inventory_id'),
+
+                            TextInput::make('name')
                                 ->label(__('Inventory'))
-                                ->options(Inventory::pluck('name', 'id')), 
+                                ->disabled(),
 
                             TextInput::make('before')
                                 ->label(__('Opening'))
                                 ->numeric()
-                                ->default(35)
                                 ->disabled(),
 
                             TextInput::make('sold')
                                 ->label(__('Sold'))
                                 ->numeric()
-                                ->default(8)
                                 ->disabled(),
 
                             TextInput::make('restock')
                                 ->label(__('Restock'))
                                 ->numeric()
+                                ->reactive()
                                 ->default(0),
 
                             TextInput::make('damaged')
                                 ->label(__('Damaged'))
                                 ->numeric()
+                                ->reactive()
                                 ->default(0),
 
                             TextInput::make('after')
